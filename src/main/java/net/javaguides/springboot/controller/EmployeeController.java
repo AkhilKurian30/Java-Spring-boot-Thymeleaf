303aa21fac0b80c8ccc3c5de3faa90535fb050c2
Akhil Kurian <akurian@menervasoftware.com>
2021-07-29 08:16:29 +0530

    newly added

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -21,11 +21,18 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	// display list of employees
+
+
 	@GetMapping("/")
-	public String viewHomePage(Model model) {
-		return findPaginated(1, "firstName", "asc", model);		
-	}
+    public String viewHomePage(Model model) {
+        model.addAttribute("listemployees", employeeService.getAllEmployees());
+        return "index";
+    }
+	// display list of employees
+	// @GetMapping("/")
+	// public String viewHomePage(Model model) {
+	// 	return findPaginated(1, "firstName", "asc", model);		
+	// }
 	
 	@GetMapping("/showNewEmployeeForm")
 	public String showNewEmployeeForm(Model model) {
